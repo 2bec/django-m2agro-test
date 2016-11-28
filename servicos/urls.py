0baa36c6c636eba5d8b2fc24f2210a4c2982fab3
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-11-27 23:49:35 -0200

    Better urls

--- servicos/urls.py
+++ servicos/urls.py
@@ -4,8 +4,7 @@ from django.views.generic import TemplateView
 from servicos.views import ServicosList, ServicoDetail
 
 urlpatterns = [
-	url(r'^$', TemplateView.as_view(template_name="safras/home.html")),
     # api
-    url(r'^api/v1/$', ServicosList.as_view(), name="servicos_list"),
-    url(r'^api/v1/(?P<pk>[0-9]+)/$', ServicoDetail.as_view(), name="servico_detail"),
+    url(r'^$', ServicosList.as_view(), name="servicos_list"),
+    url(r'^(?P<pk>[0-9]+)/$', ServicoDetail.as_view(), name="servico_detail"),
 ]
