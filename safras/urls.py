09d057515411cae4bcb38672d36c64dc7d38275c
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-11-27 23:57:39 -0200

    Better urls

--- safras/urls.py
+++ safras/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
 
 from safras.views import SafrasList, SafraDetail
 
 urlpatterns = [
-	url(r'^$', TemplateView.as_view(template_name="safras/home.html")),
     # api
-    url(r'^api/v1/$', SafrasList.as_view(), name="safras_list"),
-    url(r'^api/v1/(?P<pk>[0-9]+)/$', SafraDetail.as_view(), name="safra_detail")
+    url(r'^$', SafrasList.as_view(), name="safras_list"),
+    url(r'^(?P<pk>[0-9]+)/$', SafraDetail.as_view(), name="safra_detail")
 ]
