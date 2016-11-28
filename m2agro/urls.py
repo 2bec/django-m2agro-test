33b98d769cf589eb30b3d6f79d9284fe824289d5
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-11-27 23:59:26 -0200

    Added an example home page and improvements on api urls

--- m2agro/urls.py
+++ m2agro/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^admin/', admin.site.urls),
-    url(r'^produtos/', include("produtos.urls")),
-    url(r'^safras/', include("safras.urls")),
-    url(r'^servicos/', include("servicos.urls")),
+    url(r'^api/v1/produtos/', include("produtos.urls")),
+    url(r'^api/v1/safras/', include("safras.urls")),
+    url(r'^api/v1/servicos/', include("servicos.urls")),
 ]
