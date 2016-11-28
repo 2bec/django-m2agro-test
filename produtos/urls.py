5abf7e737275e06d76d0f37c53ed42473ce80ed3
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-11-28 14:03:30 -0200

    Added preco medio url

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from produtos.views import ProdutosList, ProdutoDetail
+from produtos.views import ProdutosList, ProdutoDetail, PrecoMedioUpdate
 
 urlpatterns = [
     # api
     url(r'^$', ProdutosList.as_view(), name="produtos_list"),
+    url(r'^update/preco_medio/$', PrecoMedioUpdate.as_view(), name="preco_medio_update"),
     url(r'^(?P<pk>[0-9]+)/$', ProdutoDetail.as_view(), name="produto_detail")
 ]
