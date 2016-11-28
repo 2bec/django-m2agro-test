778d9385f43d14a7b71175881bf14d8f4e51d289
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-11-28 13:57:24 -0200

    Dummy commit

--- servicos/views.py
+++ servicos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 
 from rest_framework import status
 from rest_framework.views import APIView
@@ -49,6 +49,3 @@ class ServicoDetail(APIView):
         servico = self.get_object(pk)
         servico.delete()
         return Response(servico)
-
-
-    
