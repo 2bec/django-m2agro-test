75ada9b16311da61d431c652fe8991ca89f7f335
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-11-27 23:53:18 -0200

    Better urls

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
 
 from produtos.views import ProdutosList, ProdutoDetail
 
 urlpatterns = [
-	url(r'^$', TemplateView.as_view(template_name="produtos/home.html")),
     # api
-    url(r'^api/v1/$', ProdutosList.as_view(), name="produtos_list"),
-    url(r'^api/v1/(?P<pk>[0-9]+)/$', ProdutoDetail.as_view(), name="produto_detail")
+    url(r'^$', ProdutosList.as_view(), name="produtos_list"),
+    url(r'^(?P<pk>[0-9]+)/$', ProdutoDetail.as_view(), name="produto_detail")
 ]
