d0b581df9670b40817401437947b8e47bad2eadd
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-11-28 14:03:03 -0200

    Added new preco medio serializers

--- produtos/serializers.py
+++ produtos/serializers.py
@@ -8,3 +8,14 @@ class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Produto
         fields = ('pk', 'nome', 'preco_medio', 'created', 'updated',"is_active")
+
+
+class PrecoMedioSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Produto
+        fields = ('nome', 'preco_medio', 'updated')
+
+class PrecoMedioMensalSerializer(serializers.Serializer):
+    mes = serializers.IntegerField()
+    ano = serializers.IntegerField()
